95965caf20cdd284192a2b19dee34ec170a4b5d4
delvh <leon@kske.dev>
2024-03-04 00:31:41 +0100

    Remove commented out code

--- misarch-catalog.tf
+++ misarch-catalog.tf
@@ -69,13 +69,6 @@ resource "kubernetes_deployment" "misarch_catalog" {
             name  = "SPRING_R2DBC_PASSWORD"
             value = random_password.misarch_catalog_db_password.result
           }
-
-          # liveness_probe {
-          #   http_get {
-          #     path = "/graphiql"
-          #     port = 8080
-          #   }
-          # }
         }
       }
     }
