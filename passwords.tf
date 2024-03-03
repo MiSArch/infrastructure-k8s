7da804eaf7500e36f08f78bd71e64c3d535c1041
delvh <leon@kske.dev>
2024-03-04 00:40:51 +0100

    Remove unneeded passwords

--- passwords.tf
+++ passwords.tf
@@ -12,18 +12,3 @@ resource "random_password" "misarch_catalog_db_password" {
   length  = 32
   special = false
 }
-
-resource "random_password" "misarch_catalog_db_pgpool_password" {
-  length  = 32
-  special = false
-}
-
-resource "random_password" "misarch_catalog_db_repmgr_password" {
-  length  = 32
-  special = false
-}
-
-resource "random_password" "misarch_catalog_db_postgres_user_password" {
-  length  = 32
-  special = false
-}
