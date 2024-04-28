be9c5db6953bc511d78052f381fd7d517b4714e9
delvh <leon@kske.dev>
2024-04-28 16:49:20 +0200

    Add `Invoice`, `Order`, `Payment`, `Review`, `Shoppingcart`, `Wishlist` services
    This is currently missing the necessary env vars

---
+++ misarch-order.tf
@@ -0,0 +1,59 @@
+resource "kubernetes_deployment" "misarch_order" {
+  depends_on = [helm_release.misarch_order_db, terraform_data.dapr]
+  metadata {
+
+    name      = local.misarch_order_service_name
+    labels    = merge(local.base_misarch_labels, local.misarch_order_specific_labels)
+    namespace = local.namespace
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = local.misarch_order_service_name
+      }
+    }
+
+    template {
+      metadata {
+        labels      = merge(local.base_misarch_labels, local.misarch_order_specific_labels)
+        annotations = merge(local.base_misarch_annotations, local.misarch_order_specific_annotations)
+      }
+
+      spec {
+
+        container {
+          image             = "ghcr.io/misarch/order:${var.MISARCH_ORDER_VERSION}"
+          image_pull_policy = "Always"
+
+          name = local.misarch_order_service_name
+
+
+          resources {
+            limits = {
+              cpu    = "500m"
+              memory = "1200Mi"
+            }
+            requests = {
+              cpu    = "100m"
+              memory = "400Mi"
+            }
+          }
+
+          env_from {
+            config_map_ref {
+              name = local.misarch_base_env_vars_configmap
+            }
+          }
+          env_from {
+            config_map_ref {
+              name = local.misarch_order_env_vars_configmap
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ misarch-payment.tf
@@ -0,0 +1,59 @@
+resource "kubernetes_deployment" "misarch_payment" {
+  depends_on = [helm_release.misarch_payment_db, terraform_data.dapr]
+  metadata {
+
+    name      = local.misarch_payment_service_name
+    labels    = merge(local.base_misarch_labels, local.misarch_payment_specific_labels)
+    namespace = local.namespace
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = local.misarch_payment_service_name
+      }
+    }
+
+    template {
+      metadata {
+        labels      = merge(local.base_misarch_labels, local.misarch_payment_specific_labels)
+        annotations = merge(local.base_misarch_annotations, local.misarch_payment_specific_annotations)
+      }
+
+      spec {
+
+        container {
+          image             = "ghcr.io/misarch/payment:${var.MISARCH_PAYMENT_VERSION}"
+          image_pull_policy = "Always"
+
+          name = local.misarch_payment_service_name
+
+
+          resources {
+            limits = {
+              cpu    = "500m"
+              memory = "1200Mi"
+            }
+            requests = {
+              cpu    = "100m"
+              memory = "400Mi"
+            }
+          }
+
+          env_from {
+            config_map_ref {
+              name = local.misarch_base_env_vars_configmap
+            }
+          }
+          env_from {
+            config_map_ref {
+              name = local.misarch_payment_env_vars_configmap
+            }
+          }
+        }
+      }
+    }
+  }
+}
