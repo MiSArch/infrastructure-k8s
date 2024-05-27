8446dfe7cc851cef0471ae44fc45afed30573eac
delvh <leon@kske.dev>
2024-05-27 22:56:17 +0200

    Try to set authentication for RabbitMQ

--- variables-urls.tf
+++ variables-urls.tf
@@ -126,7 +126,7 @@ locals {
   payment_url     = "${local.misarch_payment_service_name}.${var.KUBERNETES_NAMESPACE}.svc.cluster.local:${local.payment_port}"
 
   minio_url     = "${local.minio_full_service_name}.${var.KUBERNETES_NAMESPACE}.svc.cluster.local:${local.minio_port}"
-  rabbitmq_url     = "${local.rabbitmq_full_service_name}.${var.KUBERNETES_NAMESPACE}.svc.cluster.local:${local.rabbitmq_port}"
+  rabbitmq_url     = "${var.MISARCH_DB_USER}:${random_password.rabbitmq_password.result}@${local.rabbitmq_full_service_name}.${var.KUBERNETES_NAMESPACE}.svc.cluster.local:${local.rabbitmq_port}"
   otel_collector_url = "${local.otel_collector_full_service_name}.${var.KUBERNETES_NAMESPACE}.svc.cluster.local:${local.otel_collector_port}"
 }
 
